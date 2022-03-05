c24abed72cceee3b1b7ad7ee70b9581ed3617cac
JackTrueax <jack.r.trueax@gmail.com>
2022-03-04 23:51:32 -0500

    initial commit

---
+++ app/src/main/java/com/example/parstagram/ParseApplication.java
@@ -0,0 +1,24 @@
+package com.example.parstagram;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("B8GCnXi8v0fW59m1GtogHyhXngaWi4bh6aqqS3rM")
+                .clientKey("Qv4BQS7ta14hTzG8Xe8nOtBAESFFdrvu3axAoxUx")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
